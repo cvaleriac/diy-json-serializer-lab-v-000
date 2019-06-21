fa6dab2e193e974460970434cf8344ab9ce03e20
valeria campilongo <cvaleriac@hotmail.it>
2019-06-21 15:45:36 +0000

    Automatically backed up by Learn

--- app/serializer/product_serializer.rb
+++ app/serializer/product_serializer.rb
@@ -1,16 +1,19 @@
-class PeoductSerializer
-  def self.serialize(product)
+class ProductSerializer
 
+  def self.serialize(product)
 
     serialized_product = '{'
 
-    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"id": '+ product.id.to_s + ', '
     serialized_product += '"name": "' + product.name + '", '
-    serialized_product += '"prize": "' + product.prize + '", '
-    serialized_product += '"inventory": "' + product.inventory + '", '
 
+    serialized_product += '"price": ' + product.price.to_s + ', '
 
+    serialized_product += '"inventory": ' + product.inventory.to_s  + ', '
 
+    serialized_product += '"description": "' + product.description + '" '
     serialized_product += '}'
+
   end
+
 end
